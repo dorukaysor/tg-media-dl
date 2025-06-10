06518cd1baacdab23667ac0ac991834cb645e88d
dantes <112493972+dorukaysor@users.noreply.github.com>
2025-06-10 12:53:25 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const BOT_NAME = "Cassy";
-export const BOT_TOKEN = "7849224850:AAE2vRd-VrT3P_sFLSBNcB11YJFgyY9r14M";
+export const BOT_TOKEN = "";
 export const MONGODB_URI = "";
 export const OWNER_NAME = "Ethan";
 export const OWNER_ID = 1958269193;
